bdc17255b3fda588a837eb399e285e683199653a
CHEN Ziang <clarence.z.chen@outlook.com>
2022-11-25 00:27:51 +0800

    加入了数据库支持：Flask-SQLAlchemy“

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask,render_template
 from markupsafe import escape
 from flask import url_for
+from flask_sqlalchemy import SQLAlchemy
+import click
+import os
+
 name = 'CHEN Ziang'
 movies = [
     {'title': 'My Neighbor Totoro', 'year': '1988'},
@@ -18,7 +22,51 @@ movies = [
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + os.path.join(app.root_path,'data.db')
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(20))
+class Movie(db.Model):
+    id = db.Column(db.Integer,primary_key = True)
+    title = db.Column(db.String(60))
+    year = db.Column(db.String(4))
+
+@app.cli.command()
+def forge():
+    db.create_all()
+    name = "CHEN Ziang"
+    movies =  [
+        {'title': 'My Neighbor Totoro', 'year': '1988'},
+        {'title': 'Dead Poets Society', 'year': '1989'},
+        {'title': 'A Perfect World', 'year': '1993'},
+        {'title': 'Leon', 'year': '1994'},
+        {'title': 'Mahjong', 'year': '1996'},
+        {'title': 'Swallowtail Butterfly', 'year': '1996'},
+        {'title': 'King of Comedy', 'year': '1999'},
+        {'title': 'Devils on the Doorstep', 'year': '1999'},
+        {'title': 'WALL-E', 'year': '2008'},
+        {'title': 'The Pork of Music', 'year': '2012'},
+    ]
+    user  = User(name = name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'],year=m['year'])
+        db.session.add(movie)
+    db.session.commit()
+    click.echo('Done.')
+
+
 @app.route("/")
+def index():
+    user = User.query.first()
+    movies = Movies.query.all()
+    return render_template("index.html",name = user,movies = movies)
+
+
+
+
 # def hello():
 #     return "<h1>My First Flask!</h1><br><h4>欢迎来到我的Watchlist!</h4>"
 # @app.route("/<name>")
@@ -29,6 +77,5 @@ app = Flask(__name__)
 #     print(url_for("hello"))
 #     print(url_for("user_page",name = "chenziang"))
 #     return "Test page"
-def index():
-    return render_template("index.html",name = name,movies = movies)
+
 
