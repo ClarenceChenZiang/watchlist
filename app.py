6d1e47ddebdc1d88ab3afde0aca541b7bdd6c2b5
CHEN Ziang <clarence.z.chen@outlook.com>
2022-11-25 16:43:07 +0800

    Add base template and 404 page

--- app.py
+++ app.py
@@ -5,20 +5,6 @@ from flask_sqlalchemy import SQLAlchemy
 import click
 import os
 
-name = 'CHEN Ziang'
-movies = [
-    {'title': 'My Neighbor Totoro', 'year': '1988'},
-    {'title': 'Dead Poets Society', 'year': '1989'},
-    {'title': 'A Perfect World', 'year': '1993'},
-    {'title': 'Leon', 'year': '1994'},
-    {'title': 'Mahjong', 'year': '1996'},
-    {'title': 'Swallowtail Butterfly', 'year': '1996'},
-    {'title': 'King of Comedy', 'year': '1999'},
-    {'title': 'Devils on the Doorstep', 'year': '1999'},
-    {'title': 'WALL-E', 'year': '2008'},
-    {'title': 'The Pork of Music', 'year': '2012'},
-]
-
 
 
 app = Flask(__name__)
@@ -28,10 +14,18 @@ db = SQLAlchemy(app)
 class User(db.Model):
     id = db.Column(db.Integer,primary_key = True)
     name = db.Column(db.String(20))
-class Movie(db.Model):
+class Movies(db.Model):
     id = db.Column(db.Integer,primary_key = True)
     title = db.Column(db.String(60))
     year = db.Column(db.String(4))
+
+@app.cli.command()
+@click.option('--drop',is_flag = True,help = "Create after drop")
+def initdb(drop):
+    if drop:
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.')
 
 @app.cli.command()
 def forge():
@@ -52,19 +46,25 @@ def forge():
     user  = User(name = name)
     db.session.add(user)
     for m in movies:
-        movie = Movie(title=m['title'],year=m['year'])
-        db.session.add(movie)
+        Movie = Movies(title=m['title'],year=m['year'])
+        db.session.add(Movie)
     db.session.commit()
     click.echo('Done.')
 
 
+
+
+@app.context_processor
+def inject_user():
+    user = User.query.first().name
+    return dict(user = user)
 @app.route("/")
 def index():
-    user = User.query.first()
-    movies = Movies.query.all()
-    return render_template("index.html",name = user,movies = movies)
-
-
+    movie = Movies.query.all()
+    return render_template("index.html",movies = movie)
+@app.errorhandler(404)
+def page_not_found(e):  # 异常对象为参数
+    return render_template('404.html'),404
 
 
 # def hello():
