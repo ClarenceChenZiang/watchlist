4a74238357671b7a9fc623de09d45cc80c6f7111
CHEN Ziang <clarence.z.chen@outlook.com>
2022-11-26 00:53:17 +0800

    create,edit and delete item by form

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask,render_template
 from markupsafe import escape
-from flask import url_for
+from flask import url_for,request,redirect,flash
 from flask_sqlalchemy import SQLAlchemy
 import click
 import os
@@ -51,17 +51,65 @@ def forge():
     db.session.commit()
     click.echo('Done.')
 
-
-
+# flash存消息进session，存进cookie，设置密钥
+app.config['SECRET_KEY'] = 'dev'
 
 @app.context_processor
 def inject_user():
     user = User.query.first().name
     return dict(user = user)
-@app.route("/")
-def index():
+
+# 1. 主页路由
+@app.route("/",methods = ['GET','POST']) # 识别post和get请求
+def index():   
+    if request.method == "POST":     # 判断是否post请求
+        title = request.form["title"]
+        year = request.form["year"]
+        # 数据验证，flash返回消息，需要设置签名密钥
+        if not title or not year or len(year)!=4 or len(title)>60:
+            flash('Invalid input.')
+            return redirect(url_for('index'))
+        # 数据入库
+        movie = Movies(title = title,year = year)
+        db.session.add(movie)
+        db.session.commit()
+        flash('item created')
+        return redirect(url_for('index'))
+    # 渲染jinjia2模板
     movie = Movies.query.all()
     return render_template("index.html",movies = movie)
+
+# 2. 编辑条目路由
+@app.route('/movie/edit/<int:movie_id>',methods=['GET','POST'])
+# 查询post过来的movie，提取表单中的title和year，并更新到数据库中
+def edit(movie_id):
+    movie = Movies.query.get_or_404(movie_id)
+    if request.method == "POST":
+        title = request.form['title']
+        year = request.form['year']
+        if not title or not year or len(year)!=4 or len(title)>60:
+            flash('Invalid input.')
+            return redirect(url_for('edit',movie_id = movie_id))
+        movie.title = title
+        movie.year = year
+        db.session.commit()
+        flash('Item Updated.')
+
+        return redirect(url_for('index'))
+    return render_template('edit.html',movie = movie)
+# 3. 删除条目路由
+@app.route("/movie/delete/<int:movie_id>",methods = ["POST"])
+def delete(movie_id):
+    movie = Movies.query.get_or_404(movie_id)
+    db.session.delete(movie)
+    db.session.commit()
+    flash("Item deleted.")
+    return redirect(url_for('index'))
+    
+
+
+
+# 404 路由
 @app.errorhandler(404)
 def page_not_found(e):  # 异常对象为参数
     return render_template('404.html'),404
